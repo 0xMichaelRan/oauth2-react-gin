968641a600d28aa28ade4a04e34e94c82f7c3092
0xMichaelRan <michaelran16@gmail.com>
2024-06-02 01:19:29 +0100

    log in from login.js

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Footer from '../components/footer';
 import { createGlobalStyle } from 'styled-components';
 
@@ -15,49 +16,82 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
-const logintwo = () => (
-  <div>
-    <GlobalStyles />
+const logintwo = () => {
 
-    <section className='jumbotron breadcumb no-bg' style={{ backgroundImage: `url(${'./img/background/10.jpg'})` }}>
-      <div className='mainbreadcumb'>
-        <div className='container'>
-          <div className='row align-items-center px-0'>
-            <div className="col-lg-4 offset-lg-4 m-auto px-0">
-              <div className="box-login">
-                <h3 className="mb10">Sign In</h3>
-                <p>Login using an existing account or create a new account <span>here</span>.</p>
-                <form name="contactForm" id='contact_form' className="form-border" action='#'>
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-                  <div className="field-set">
-                    <input type='text' name='email' id='email' className="form-control" placeholder="username" />
-                  </div>
+    // Perform POST request to 'http://localhost:9000/v1/user/login' using axios
+    axios.post('http://localhost:9000/v1/user/login', {
+      email: e.target.email.value,
+      password: e.target.password.value
+    })
+      .then(response => {
+        // Handle successful response
+        console.log(response.data);
 
-                  <div className="field-set">
-                    <input type='password' name='password' id='password' className="form-control" placeholder="password" />
-                  </div>
+        // check response.data.message and decide if this is a successful login
+        // if successful, redirect to home page
+        // if unsuccessful, show an error message
+        if (response.data.message === "Successfully logged in") {
+          // display an unblocking message "welcome" and quickly redirect to home page
+          window.location.href = "/home1";
+        } else {
+          // Show error message
+          alert("Login failed. Please try again.");
+        }
+        
+      })
+      .catch(error => {
+        // Handle error
+        console.error(error);
+      });
+  }
 
-                  <div className="field-set">
-                    <input type='submit' id='send_message' value='Submit' className="btn btn-main btn-fullwidth color-2" />
-                  </div>
+  return (
+    <div>
+      <GlobalStyles />
 
-                  <div className="clearfix"></div>
-                  <ul className="list s3">
-                    <li>Login with:</li>
-                    <li><span >Facebook</span></li>
-                    <li><span >Google</span></li>
-                  </ul>
-                  <div className="spacer-half"></div>
-                </form>
+      <section className='jumbotron breadcumb no-bg' style={{ backgroundImage: `url(${'./img/background/10.jpg'})` }}>
+        <div className='mainbreadcumb'>
+          <div className='container'>
+            <div className='row align-items-center px-0'>
+              <div className="col-lg-4 offset-lg-4 m-auto px-0">
+                <div className="box-login">
+                  <h3 className="mb10">Sign In</h3>
+                  <p>Login using an existing account or create a new account <span>here</span>.</p>
+                  <form name="contactForm" id='contact_form' className="form-border" action='#' onSubmit={handleSubmit}>
+
+                    <div className="field-set">
+                      <input type='text' name='email' id='email' className="form-control" placeholder="username" />
+                    </div>
+
+                    <div className="field-set">
+                      <input type='password' name='password' id='password' className="form-control" placeholder="password" />
+                    </div>
+
+                    <div className="field-set">
+                      <input type='submit' id='send_message' value='Submit' className="btn btn-main btn-fullwidth color-2" />
+                    </div>
+
+                    <div className="clearfix"></div>
+                    <ul className="list s3">
+                      <li>Login with:</li>
+                      <li><span >Facebook</span></li>
+                      <li><span >Google</span></li>
+                    </ul>
+                    <div className="spacer-half"></div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
 
-    <Footer />
-  </div>
+      <Footer />
+    </div>
+  );
+}
 
-);
 export default logintwo;
