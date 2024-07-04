9fe6421efd76bef0e8a6618eeb2a49aa1a5f853e
0xMichaelRan <michaelran16@gmail.com>
2024-07-04 05:41:11 +0100

    change from npm to pnpm

--- src/components/components/FeatureBox.js
+++ src/components/components/FeatureBox.js
@@ -42,7 +42,7 @@ const featurebox = () => (
         </Reveal>
         <div className="text">
           <Reveal className='onStep' keyframes={fadeInUp} delay={100} duration={600} triggerOnce>
-            <h4 className="">Build Your Reputation</h4>
+            <h4 className="">Share to Friends</h4>
           </Reveal>
           <Reveal className='onStep' keyframes={fadeInUp} delay={200} duration={600} triggerOnce>
             <p className="">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem.</p>
@@ -59,7 +59,7 @@ const featurebox = () => (
         </Reveal>
         <div className="text">
           <Reveal className='onStep' keyframes={fadeInUp} delay={100} duration={600} triggerOnce>
-            <h4 className="">Get Rewarded in Tokens</h4>
+            <h4 className="">Get Paid in Tokens</h4>
           </Reveal>
           <Reveal className='onStep' keyframes={fadeInUp} delay={200} duration={600} triggerOnce>
             <p className="">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem.</p>
